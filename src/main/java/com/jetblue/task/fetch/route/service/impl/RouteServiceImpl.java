081fcb71690059df903649c7f89be7e949950d95
Pankhuri2909 <58258674+Pankhuri2909@users.noreply.github.com>
2020-02-26 12:36:50 +0530

    Update RouteServiceImpl.java
    Added a static list to hold all the routes.

--- src/main/java/com/jetblue/task/fetch/route/service/impl/RouteServiceImpl.java
+++ src/main/java/com/jetblue/task/fetch/route/service/impl/RouteServiceImpl.java
@@ -14,6 +14,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class RouteServiceImpl implements RouteService {
+    
+    private static List<Route> routeList;
 
     @Autowired
     RouteDao routeDao;
@@ -21,7 +23,7 @@ public class RouteServiceImpl implements RouteService {
     @Transactional
     @Override
     public List<Route> get() {
-        return loadList();
+        return routeList;
     }
 
     public List<Route> getSpecificRoute(@PathVariable(value = "name") String city) {
@@ -38,7 +40,7 @@ public class RouteServiceImpl implements RouteService {
     }
 
     @PostConstruct
-    public List<Route> loadList(){
-        return routeDao.getAllRoutes();
+    public void loadList(){
+        routeList = routeDao.getAllRoutes();
     }
 }
